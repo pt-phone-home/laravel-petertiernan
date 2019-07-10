f4aa41d0e4e8a42d93bd7b6fdcbdd21932ee84bc
Peter Tiernan <ptiernan@gmail.com>
2019-07-10 23:12:12 +0100

    adding google tracking

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,6 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125112334-7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-125112334-7');
+</script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
